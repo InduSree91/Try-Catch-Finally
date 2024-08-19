5c795ec84beceaba767bd1288118ea81075693f6
Indu Sree <Indu Sree@DESKTOP-UN8GH21>
2024-08-19 13:22:20 +0530

    Add project files.

---
+++ TryCatchFinally/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TryCatchFinally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start the code");
+
+            int i = 0;
+
+            try
+            {
+                int result = 10 / i;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Number can not be divided by Zero");
+            }
+            finally
+            {
+                Console.WriteLine("Completed running");
+            }
+        }
+    }
+}
